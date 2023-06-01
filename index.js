e4bd20761daad2be829bb8cd4881759a36994619
Cong Le <congldqn888@gmail.com>
2023-06-01 21:00:39 +0700

    init project

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routers/user";
+import productRouter from "./routers/product";
+import categoryRouter from "./routers/category";
+import bodyParser from "body-parser";
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
+app.use(cors());
+
+mongoose.connect("mongodb://localhost:27017/asm-web501");
+
+export const viteNodeApp = app;
