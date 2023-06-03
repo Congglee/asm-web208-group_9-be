111296dbd872c1307ce862c66974cd957f9dde5b
Cong Le <congldqn888@gmail.com>
2023-06-03 15:26:43 +0700

    hotfix: setup base project refs: AG9W-1

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routers/user";
-import productRouter from "./routers/product";
-import categoryRouter from "./routers/category";
 import bodyParser from "body-parser";
 dotenv.config();
 
@@ -14,6 +11,8 @@ app.use(bodyParser.json({ limit: "5mb" }));
 app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
 app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/asm-web208-g7");
+mongoose.connect(
+  "mongodb+srv://congltph27602:congltph27602@asm-web208.6mtrvgz.mongodb.net/asm-web208-g7"
+);
 
 export const viteNodeApp = app;
