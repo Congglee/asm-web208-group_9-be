c858c0d9e22503a8f302393eaa8f8cf02aa18435
Cong Le <congldqn888@gmail.com>
2023-06-04 14:22:03 +0700

    hotfix: design model refs: AG9W-10

---
+++ models/cart.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+var cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    cartItems: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: { type: Number, default: 1 },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Cart", cartSchema);
