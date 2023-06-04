6491ebcc3ea8b757b5641421887a33181a445715
Nguyễn Viết Quân <116721150+quandev99@users.noreply.github.com>
2023-06-04 12:57:19 +0700

    Revert "feature:[BE] Authentication (Register & Login) refs:AG9W-8"

--- index.js
+++ index.js
@@ -3,18 +3,14 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-// Auth
-import userRoute from "./routers/user";
 dotenv.config();
+
 const app = express();
 
 app.use(bodyParser.json({ limit: "5mb" }));
 app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
-app.use(cookieParser());
 app.use(cors());
 
-app.use("/api/user", userRoute);
 mongoose.connect(
   "mongodb+srv://congltph27602:congltph27602@asm-web208.6mtrvgz.mongodb.net/asm-web208-g7"
 );
